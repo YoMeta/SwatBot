69f3ac445da6f5cd32b41cf3312dc9975e9115db
YoMeta <45538506+YoMeta@users.noreply.github.com>
2019-01-02 13:35:53 +0100

    Update botswat.js

--- .gitignore/botswat.js
+++ .gitignore/botswat.js
@@ -145,34 +145,42 @@ bot.on('message', message => {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
             }
+        }
         if(heure === 18) {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
             }
+        }
         if(heure === 21) {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
             }
+        }    
         if(heure === 0) {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
             }
+        }
         if(heure === 3) {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
             }
+        }
         if(heure === 6) {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
             }
+        }
         if(heure === 9) {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
-            } 
+            }
+        } 
         if(heure === 12) {
             if(min === 0){
                 message.channel.get("général").send('On répète mais ça nous aide: ça prend deux secondes pour nous envoyer au sommet! \n https://scum.top-serveurs.net/fr-18-les-con-damnes-zones-de-pvp-d1-d2-d3-d4-discord-httpsdiscordggn6ssg7w');
-            }            
+            }
+        }
        }, 60000);
     }
 }); 
